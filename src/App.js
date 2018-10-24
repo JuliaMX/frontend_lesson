f9e90053350e442f913dde24f22e7bc7b16c6c46
JuliaMX <jam@photosides.ru>
2018-10-24 23:17:27 +0300

    -

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+    state = {
+        data: [],
+        id: 0,
+        message: null,
+        intervalIsSet: false
+    };
+
+
+    componentDidMount() {
+        this.getDataFromDb();
+        if (!this.state.intervalIsSet) {
+            let interval = setInterval(this.getDataFromDb, 1000);
+            this.setState({ intervalIsSet: interval });
+        }
+    }
+
+
+    componentWillUnmount() {
+        if (this.state.intervalIsSet) {
+            clearInterval(this.state.intervalIsSet);
+            this.setState({ intervalIsSet: null });
+        }
+    }
+
+
+    getDataFromDb = () => {
+        fetch("/users")
+            .then(data => data.json())
+            .then(res => this.setState({ data: res.data }));
+    };
+
+
   render() {
+
+   const { data } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+        <div>
+
+            {data.length <= 0 ? "NO DB ENTRIES YET" : data.map(dat => (
+                <p style={{ padding: "10px" }} key={data.message}>
+                    <span style={{ color: "gray" }}> id: </span> {dat._id} <br />
+                    <span style={{ color: "gray" }}> name: </span> {dat.name} <br />
+                    <span style={{ color: "gray" }}> age: </span> {dat.age} <br />
+
+                </p>
+            ))}
+
+        </div>
+
     );
   }
 }
