16029b0b7712e453310d0b6d67c67075c1462458
JuliaMX <jam@photosides.ru>
2018-10-31 10:29:18 +0200

    -

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
 
 
@@ -8,7 +9,10 @@ class App extends Component {
         data: [],
         id: 0,
         message: null,
-        intervalIsSet: false
+        intervalIsSet: false,
+        name: null,
+        age: 0
+
     };
 
 
@@ -36,25 +40,53 @@ class App extends Component {
     };
 
 
+    postDataToDB = (name, age) => {
+        fetch("/users", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: name,
+                age: age
+            })
+        }).then(function(response) {
+            return response.json()
+        }).then(function(body) {
+            console.log(body);
+        });
+    };
+
+
   render() {
-
    const { data } = this.state;
-
     return (
-
         <div>
-
+            <div>
+                <input
+                    type="text"
+                    placeholder="add name"
+                    onChange={e => this.setState({name:  e.target.value })}
+                /> <br/>
+                <input
+                    type="text"
+                    placeholder="add age"
+                    onChange={e => this.setState({ age: e.target.value })}
+                />
+                <button onClick={() => this.postDataToDB(this.state.name, this.state.age)}>
+                    ADD
+                </button>
+            </div>
             {data.length <= 0 ? "NO DB ENTRIES YET" : data.map(dat => (
                 <p style={{ padding: "10px" }} key={data.message}>
+                    {/*<span style={{ color: "gray" }}> doc: </span> {dat.name} {dat.age} {dat.createdAt} <br />*/}
                     <span style={{ color: "gray" }}> id: </span> {dat._id} <br />
                     <span style={{ color: "gray" }}> name: </span> {dat.name} <br />
                     <span style={{ color: "gray" }}> age: </span> {dat.age} <br />
-
                 </p>
             ))}
-
         </div>
-
     );
   }
 }
